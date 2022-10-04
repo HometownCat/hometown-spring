ab23c4797bf94cc22f10c47d1abedc44218fa72a
uiurihappy <uiurihappy@sju.ac.kr>
2022-10-04 14:57:52 +0900

    feat: model definition

--- src/main/java/com/example/hometown/hometown/domain/BoardLike.java
+++ src/main/java/com/example/hometown/hometown/domain/BoardLike.java
@@ -27,4 +27,8 @@ public class BoardLike {
 	@ManyToOne
 	@JoinColumn(name = "boardId", referencedColumnName = "id")
 	private Board board;
+
+	@ManyToOne
+	@JoinColumn(name = "userId", referencedColumnName = "id")
+	private User user;
 }
